d85f0e85f33c3e6112cd7711c64b26aef303eb21
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-06-03 16:31:34 -0400

    clue 6 solved

--- find_carmen.sql
+++ find_carmen.sql
@@ -70,6 +70,11 @@ carmen=# SELECT code, country.name, city.name FROM country JOIN city ON country.
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
 
+SELECT country.code, country.name, city.name FROM country JOIN city ON country.capital = city.id WHERE code = 'BRA';
+
+--  code |  name  |    name
+-- ------+--------+------------
+--  BRA  | Brazil | Bras�lia
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
