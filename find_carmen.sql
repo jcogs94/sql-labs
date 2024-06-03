c9ec0b9667ae3c4b17f6939dfea8336c4177c74f
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-06-03 16:23:22 -0400

    clue 5 solved

--- find_carmen.sql
+++ find_carmen.sql
@@ -61,6 +61,12 @@ SELECT code, city.name FROM country JOIN city ON country.code = city.countrycode
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
+carmen=# SELECT code, country.name, city.name FROM country JOIN city ON country.code = city.countrycode WHERE country.region = 'South America' AND city.name LIKE 'Serr%';
+
+--  code |  name  | name
+-- ------+--------+-------
+--  BRA  | Brazil | Serra
+
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
 
